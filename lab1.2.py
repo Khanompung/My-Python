cabdc786600829a3f88501658fb2bdb873af929e
paphonKhuptaphan <paphonKhuptaphan>
2024-06-10 16:36:35 +0700

    first commit

---
+++ lab1.2.py
@@ -0,0 +1,5 @@
+print("หาพื้นที่สามเหลี่ยม")
+Base = float(input("รับค่าฐาน:"))
+Height = float(input("รับค่าสูง:"))
+Area = 1/2*Base*Height
+print("คำตอบ"+str(Area))
---
+++ lab1.3.py
@@ -0,0 +1,4 @@
+print("หาพื้นที่วงกลม")
+R = float(input("รับค่ารัศมี:"))
+Area = 3.14*(pow(R,2))
+print("คำตอบ "+str(Area))
