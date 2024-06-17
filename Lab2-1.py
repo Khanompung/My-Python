15c1934b0dd705a3b2ab181bce0c770538310854
paphonKhuptaphan <paphonKhuptaphan>
2024-06-17 14:04:29 +0700

    update

---
+++ Lab2-1.py
@@ -0,0 +1,6 @@
+print("คะแนน")
+S = int(input("คะแนนของคุณ:"))
+if S >= 60:
+    print("ผ่าน")
+else:
+    print("ไม่ผ่าน")
---
+++ Lab2.2.py
@@ -0,0 +1,14 @@
+print("คะแนน")
+S = int(input("คะแนนของคุณ:"))
+if S >= 80 and S <= 100:
+    print("เกรด A")
+elif S >= 70 and S <= 79:
+    print("เกรด B")
+elif S >= 60 and S <= 69:
+    print("เกรด C")
+elif S >= 50 and S <= 59:
+    print("เกรด D")
+elif S >= 0 and S <= 49:
+    print("เกรด F")    
+else:
+    print("กรุณากรอกข้อมูลใหม่ให้ถูกต้อง 0-100")
